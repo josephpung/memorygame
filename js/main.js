b9045d1dd556dbf34d49c66f8515370be456f1be
josephpung <goldenpummel@live.com>
2017-09-10 01:44:56 +0800

    Added reset button

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var cards = [
 	cardImage : "images/king-of-diamonds.png"
 }
 ];
-
+/////////////////////////////////CARD MATCH CHECK ////////////////////////////////////////////
 var cardsInPlay = [];
 var checkForMatch = function ()
 {	
@@ -43,7 +43,7 @@ var checkForMatch = function ()
 }
 
 };
-
+///////////////////////////////// CARD FLIP ////////////////////////////////////////////
 var flipCard = function ()
 {
 
@@ -62,20 +62,42 @@ checkForMatch();
 
 };
 
-
+////////////////////// CARD IMAGE CREATION //////////////////////
 
 var createBoard = function () {
 
 	for (var i = 0; i <cards.length; i++)
 	{
-		var cardElement = document.createElement('img');
-		cardElement.setAttribute("src","images/back.png");
-		cardElement.setAttribute("data-id",i);
-		cardElement.addEventListener('click', flipCard);
-		document.getElementById("game-board").appendChild(cardElement);
+		var cardElement = document.createElement('img');  // Assign images to cardElement to hold them
+		cardElement.setAttribute("src","images/back.png"); // set attribute for back of cards
+		cardElement.setAttribute("data-id",i); // set attribute for data ID to link each card to card in cards array.
+		cardElement.addEventListener('click', flipCard); // set on click event
+		document.getElementById("game-board").appendChild(cardElement); // add card to game-board element via appendChild 
 	}
 };
 
 createBoard();
+
+/////////////////////////// RESET BUTTON /////////////////////////////////
+
+var resetGame = function ()
+{	
+	var images = document.getElementsByTagName('img'); //create variable to hold existing images
+	var l = images.length; // Assign variable due to liveNodes. Unable to use usual method of .length
+    for (var i = 0; i < l; i++)
+ {
+    images[0].parentNode.removeChild(images[0]); //delete 1st image in variable list
+}
+	createBoard(); // restart initial card setup
+	cardsInPlay = []; //reset card check array
+	
+};
+
+document.getElementsByTagName("button")[0].addEventListener('click', resetGame);
+
+
+
+
+
 
 
