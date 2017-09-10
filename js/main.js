b1d17f227bcc9f6d0938361ba774b27dc2290adc
josephpung <goldenpummel@live.com>
2017-09-10 16:47:58 +0800

    Added Score function, more details in comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  console.log("Up and running!");
-
+var points = 0;
 var cards = [
 {
 	rank : "queen",
@@ -29,33 +29,38 @@ var cardsInPlay = [];
 var checkForMatch = function ()
 {	
 	
+	
 	if (cardsInPlay.length === 2)
 {
 
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
+		points += 1;  // add points if match found
+		
 	}
 	else
 	{
 		alert("Sorry, Try again");
 	}
-
+	
 }
+document.getElementById("gameScore").innerHTML = points; //update gameScore div with new points
 
 };
 ///////////////////////////////// CARD FLIP ////////////////////////////////////////////
 var flipCard = function ()
 {
 
-	var cardID = this.getAttribute("data-id");
+var cardID = this.getAttribute("data-id"); // link cardID to position of card clicked, in turn choosing the card in the intial array
 
-cardsInPlay.push(cards[cardID].rank);
+cardsInPlay.push(cards[cardID].rank); //push rank to cardsInPlay array
 
-console.log("User flipped "+ cards[cardID].rank);
+console.log("User flipped "+ cards[cardID].rank);///console logs for events
 console.log(cards[cardID].cardImage);
 console.log(cards[cardID].suit);
 
-this.setAttribute("src",cards[cardID].cardImage);
+this.setAttribute("src",cards[cardID].cardImage); // set image source to selected image front
+
 
 
 checkForMatch();
@@ -73,6 +78,7 @@ var createBoard = function () {
 		cardElement.setAttribute("data-id",i); // set attribute for data ID to link each card to card in cards array.
 		cardElement.addEventListener('click', flipCard); // set on click event
 		document.getElementById("game-board").appendChild(cardElement); // add card to game-board element via appendChild 
+		
 	}
 };
 
